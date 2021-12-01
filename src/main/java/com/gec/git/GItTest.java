a24a0d06d391339f1a56ae1cdf52dc7f78e8c0fe
UserManZhou <89828765+UserManZhou@users.noreply.github.com>
2021-12-01 19:59:03 +0800

    pull

--- src/main/java/com/gec/git/GItTest.java
+++ src/main/java/com/gec/git/GItTest.java
@@ -4,6 +4,7 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 2");
-
+        System.out.println("hello git 3");
+        System.out.println("hello git 4");
     }
 }
