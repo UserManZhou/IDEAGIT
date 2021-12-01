0cf36e276d834a03593775a035de0f4dda14a518
ZhouHui <2830634496@qq.com>
2021-12-01 19:53:36 +0800

    "test push"

--- src/main/java/com/gec/git/GItTest.java
+++ src/main/java/com/gec/git/GItTest.java
@@ -3,5 +3,7 @@ package com.gec.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git 2");
+
     }
 }
